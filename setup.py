ad1562f1499c5c50994fd3f949f84afb3d12be17
Luke Welden <lukewelden@hotmail.co.uk>
2022-01-23 16:50:18 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+  readme = f.read()
+  
+  setup(
+    name='hr',
+    version='1.0.0',
+    description='Command line user export utility',
+    long_description=readme,
+    author='Luke Welden',
+    author_email='lukewelden@hotmail.co.uk',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    entry_points={'console_scripts': 'hr=hr.cli:main',}
+  )
